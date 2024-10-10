db0dc6579e844776b253d0c0d96097c5e7bdb341
Distroff <Teryokhin@yandex.ru>
2024-10-10 17:11:12 +0300

    added new files

--- conftest.py
+++ conftest.py
@@ -1,11 +1,25 @@
+import time
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
+
+    browser.get('https://stepik.org')
+    # time.sleep(100)
+    link_auth = browser.find_element(By.CLASS_NAME, 'navbar__auth_login').get_attribute('href')
+    browser.get(link_auth)
+    time.sleep(5)
+    browser.find_element(By.ID, "id_login_email").send_keys('****')
+    browser.find_element(By.ID, "id_login_password").send_keys('****')
+    browser.find_element(By.CLASS_NAME, 'sign-form__btn').click()
+    print('ну, что авторизовались?')
+    time.sleep(5)
+
     yield browser
     print("\nquit browser..")
     browser.quit()
--- test_conftest.py
+++ test_conftest.py
@@ -1,29 +1,36 @@
 from selenium.webdriver.common.by import By
 import time
-import time
 import math
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-answer = math.log(int(time.time()))
-
-link_start = "https://stepik.org/lesson/236895/step/1"
-lesson_id_list = [236895, 236896, 236897, 236898, 236899, 236903, 236904,236905]
-
+# link_start = "https://stepik.org/lesson/236895/step/1"
+lesson_id_list = [236895, 236896, 236897, 236898, 236899, 236903, 236904, 236905]
+ans = []
 
 @pytest.mark.parametrize('lesson_id', lesson_id_list)
 def test_auth_resolve(browser, lesson_id):
-    browser.get(link_start)
 
-    link_auth = browser.find_element(By.ID, 'ember458').get_attribute('href')
-    browser.get(link_auth)
-
-    browser.find_element(By.ID, "id_login_email").send_keys('m****@mail.ru')
-    browser.find_element(By.ID, "id_login_password").send_keys('y*a')
-    browser.find_element(By.CLASS_NAME, 'sign-form__btn').click()
-
-
+    # time.sleep(2)
     link_lesson = f'https://stepik.org/lesson/{lesson_id}/step/1'
-
+    #  Go to lesson
+    # link_lesson = f'https://stepik.org/lesson/236895/step/1'
+    browser.get(link_lesson)
+    time.sleep(5)
+    # add the answer
+    browser.find_element(By.CLASS_NAME, 'ember-text-area').clear()
+    browser.find_element(By.CLASS_NAME, 'ember-text-area').send_keys(str(math.log(int(time.time()))))
+    time.sleep(2)
+    browser.find_element(By.CLASS_NAME, 'submit-submission').click()
+    time.sleep(2)
+    elem = WebDriverWait(browser, 5).until(EC.visibility_of_element_located((By.CLASS_NAME, "smart-hints__hint"))).text
+    ans.append(elem)
+    assert elem == 'Correct!'
+    # time.sleep(10)
+    # print('запуск теста')
+    a = [t for t in ans if t != 'Correct!']
+    print(*a)
 '''
 в таком виде он будет запускаться 2 раза и 2 раза авторизоваться. 
 Можно сделать фикстуру, которая запустит браузер, авторизуется и потом поехали тесты на страницах.
