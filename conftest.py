c0425abe59d7c68986cd9e0f8f0b3efde88d276a
Distroff <Teryokhin@yandex.ru>
2024-10-14 14:35:27 +0300

    added new files

--- conftest.py
+++ conftest.py
@@ -1,50 +1,21 @@
-# import time
-#
-# import pytest
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-#
-# @pytest.fixture(scope="session")
-# def browser():
-#     print("\nstart browser for test..")
-#     browser = webdriver.Chrome()
-#
-#     browser.get('https://stepik.org')
-#     # time.sleep(100)
-#     link_auth = browser.find_element(By.CLASS_NAME, 'navbar__auth_login').get_attribute('href')
-#     browser.get(link_auth)
-#     time.sleep(5)
-#     browser.find_element(By.ID, "id_login_email").send_keys('****')
-#     browser.find_element(By.ID, "id_login_password").send_keys('****')
-#     browser.find_element(By.CLASS_NAME, 'sign-form__btn').click()
-#     print('ну, что авторизовались?')
-#     time.sleep(5)
-#
-#     yield browser
-#     print("\nquit browser..")
-#     browser.quit()
-
-
+import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
-
+    parser.addoption('--language', action='store', default='ru',
+                     help="Choose language")
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options=options)
+    print("\nstart browser for test..")
+
     yield browser
     print("\nquit browser..")
     browser.quit()
+
